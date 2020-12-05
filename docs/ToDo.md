ccf55939850522d92e39a86874a469f011e12676
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2020-12-05 15:29:04 -0800

    init

---
+++ docs/ToDo.md
@@ -0,0 +1,2 @@
+ToDo - LadyShadow v4.0.0a
+
--- source/shadow.js
+++ source/shadow.js
@@ -2,7 +2,7 @@
  * {HSF}LadyShadow#7111
  * * V.4.0.0a
  * * December 5'th, 2020
- * * Developed By: "ScionSpy#1904", and "Bejebajay#1904"
+ * * Developed By: "Shadow_Spy#1904" (ScionSpy), and "Bejebajay#1904"
  * * Support Invite: https://discord.gg/9FUpBPQ
  * * * LadyShadow is a basic Discord Moderation bot with other logging features.
  * * * Shadow includes other "fun" commands, as well as being the central hub for a Server to Server "Rift" and the main place for the "Shadow RPG".
